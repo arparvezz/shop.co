1eb96345d07e2e4b2bf7bc4c4d8162be647afa4e
arparvezz <arparvez50@gmail.com>
2025-04-03 12:05:45 +0600

    banner-collection refactor & responsive

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-let collectionBannerItems = document.querySelectorAll('.collection-banner-item');
-
-
-collectionBannerItems.forEach(collectionBannerItem => {
-  let bgImage = collectionBannerItem.dataset.bgImg; // Get data-bg-img attribute
-  if (bgImage) {
-    collectionBannerItem.style.backgroundImage = `url(${bgImage})`;
-  }
-})
